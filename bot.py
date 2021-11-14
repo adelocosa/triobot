5ee3da89552bc46b5d1a07724bf8d4512adaf436
prettypositive <jmquander@gmail.com>
2021-11-14 13:05:31 -0500

    logging

--- bot.py
+++ bot.py
@@ -1,31 +1,33 @@
 import logging
 import os
-
+import sys
 from dotenv import load_dotenv
 
 import discord
 
-logger = logging.getLogger()
-logger.setLevel(logging.INFO)
-
-console_log = logging.StreamHandler()
+console_log = logging.StreamHandler(sys.stdout)
+console_log.setLevel(logging.INFO)
 console_log.setFormatter(
     logging.Formatter("%(asctime)s:%(levelname)s:%(name)s: %(message)s")
 )
-logger.addHandler(console_log)
 
-file_log = logging.FileHandler("log.txt")
+file_log = logging.FileHandler("debug.log")
+file_log.setLevel(logging.DEBUG)
 file_log.setFormatter(
     logging.Formatter("%(asctime)s:%(levelname)s:%(name)s: %(message)s")
 )
-logger.addHandler(file_log)
+
+log = logging.getLogger()
+log.setLevel(logging.DEBUG)
+log.addHandler(console_log)
+log.addHandler(file_log)
 
 
 def main():
     load_dotenv()
     TOKEN = os.environ.get("BOT_TOKEN")
     assert isinstance(TOKEN, str)
-    logger.info("Token found. Initializing bot...")
+    log.info("Token found. Initializing bot...")
     bot = discord.Client(TOKEN)
     bot.connect()
 
@@ -33,6 +35,6 @@ def main():
 try:
     main()
 except KeyboardInterrupt:
-    logger.info("Program halted due to keyboard interrupt.")
+    log.info("Program halted due to keyboard interrupt.")
 except Exception as e:
-    logger.exception("Program halted due to unhandled exception:")
+    log.exception("Program halted due to unhandled exception:")
