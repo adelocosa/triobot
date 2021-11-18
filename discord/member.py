40caf5837da39af2875cf800812813dee5afcd70
prettypositive <jmquander@gmail.com>
2021-11-18 09:55:50 -0500

    tracking user state

--- discord/member.py
+++ discord/member.py
@@ -1,2 +1,28 @@
+from __future__ import annotations
+
+from typing import Any, TYPE_CHECKING
+
+from .user import User
+
+if TYPE_CHECKING:
+    from .client import Client
+
+
 class GuildMember:
-    pass
+    def __init__(self, client: Client, data: dict[str, Any]):
+        self.client = client
+        self.muted: bool = data["mute"]
+        self.deafened: bool = data["deaf"]
+        self.nick: None | str = data.get("nick", None)
+        if data["user"]["id"] not in self.client.users:
+            user = User(data["user"])
+            self.client.users[user.id] = user
+        else:
+            user = self.client.users[data["user"]["id"]]
+        self.user: User = user
+
+    def __str__(self) -> str:
+        if self.nick:
+            return self.nick
+        else:
+            return self.user.username
--- discord/user.py
+++ discord/user.py
@@ -1,2 +1,11 @@
+from typing import Any
+
+
+from typing import Any
+
+
 class User:
-    pass
+    def __init__(self, data: dict[str, Any]):
+        self.id: int = int(data["id"])
+        self.username: str = data["username"]
+        self.number: int = int(data["discriminator"])
