a8498210bd6ef6618e8f7bc157ff2ee27e4257bf
prettypositive <jmquander@gmail.com>
2021-11-18 10:52:20 -0500

    pass guild instead of client

--- discord/member.py
+++ discord/member.py
@@ -1,25 +1,31 @@
 from __future__ import annotations
+import logging
 
 from typing import Any, TYPE_CHECKING
 
 from .user import User
 
 if TYPE_CHECKING:
-    from .client import Client
+    from .guild import Guild
+
+log = logging.getLogger(__name__)
 
 
 class GuildMember:
-    def __init__(self, client: Client, data: dict[str, Any]):
-        self.client = client
+    def __init__(self, guild: Guild, data: dict[str, Any]):
+        self.guild = guild
         self.muted: bool = data["mute"]
         self.deafened: bool = data["deaf"]
         self.nick: None | str = data.get("nick", None)
-        if data["user"]["id"] not in self.client.users:
+        if int(data["user"]["id"]) not in self.guild.client.users:
             user = User(data["user"])
-            self.client.users[user.id] = user
+            self.guild.client.users[user.id] = user
         else:
-            user = self.client.users[data["user"]["id"]]
+            user = self.guild.client.users[int(data["user"]["id"])]
         self.user: User = user
+        log.debug(
+            f"Added member {self.user.id} ({str(self)}) to guild {self.guild.id} ({self.guild.name})."
+        )
 
     def __str__(self) -> str:
         if self.nick:
