a79a1edf3127f0b75cf630c73a5e26f39255abc8
prettypositive <jmquander@gmail.com>
2021-11-18 09:56:00 -0500

    tracking channels

--- discord/channel.py
+++ discord/channel.py
@@ -1,10 +1,24 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, Any
+from enum import IntEnum
+
+if TYPE_CHECKING:
+    from .guild import Guild
+
+
+class ChannelType(IntEnum):
+    TEXT = 0
+    DM = 1
+    VOICE = 2
+
+
 class Channel:
-    pass
-
-
-class VoiceChannel(Channel):
-    pass
-
-
-class TextChannel(Channel):
-    pass
+    def __init__(self, guild: Guild, data: dict[str, Any]):
+        self.guild = guild
+        self.id: int = int(data["id"])
+        self.type: int = data["type"]
+        self.position: None | int = data.get("position", None)
+        self.name: None | str = data.get("name", None)
+        self.topic: None | str = data.get("topic", None)
+        self.bitrate: None | int = data.get("bitrate", None)  # in b/s
--- discord/guild.py
+++ discord/guild.py
@@ -1,12 +1,23 @@
-from typing import Any
+from __future__ import annotations
+
+from typing import Any, TYPE_CHECKING
+
+from .channel import Channel
 from .emoji import Emoji
+from .member import GuildMember
 from .http_request import HTTPRequest
+
+if TYPE_CHECKING:
+    from .client import Client
 
 
 class Guild:
-    def __init__(self, data: dict[str, Any]):
+    def __init__(self, client: Client, data: dict[str, Any]):
+        self.client = client
         self.id: int = data["id"]
         self.emojis: dict[str | int, Emoji] = self._parse_emojis(data["emojis"])
+        self.members: dict[int, GuildMember] = self._parse_members(data["members"])
+        self.channels: dict[int, Channel] = self._parse_channels(data["channels"])
 
     def _parse_emojis(self, emoji_list: list[dict]) -> dict[str | int, Emoji]:
         # emoji objects are indexed by both name and id because both seem useful
@@ -17,6 +28,20 @@ class Guild:
             emojis[emoji.id] = emoji
         return emojis
 
+    def _parse_members(self, member_list: list[dict]) -> dict[int, GuildMember]:
+        members = {}
+        for member_data in member_list:
+            member = GuildMember(self.client, member_data)
+            members[member.user.id] = member
+        return members
+
+    def _parse_channels(self, channel_list: list[dict]) -> dict[int, Channel]:
+        channels = {}
+        for channel_data in channel_list:
+            channel = Channel(self, channel_data)
+            channels[channel.id] = channel
+        return channels
+        
     async def request_emojis(self) -> dict[str | int, Emoji]:
         request = HTTPRequest()
         response = await request.list_guild_emojis(self.id)
