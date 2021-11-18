08302ada9d5286b0ccd9e9b6c2c347df6e578f56
prettypositive <jmquander@gmail.com>
2021-11-17 20:01:29 -0500

    restructured

--- discord/guild.py
+++ discord/guild.py
@@ -1,23 +1,26 @@
-from dataclasses import dataclass, field
-
+from typing import Any
 from .emoji import Emoji
 from .http_request import HTTPRequest
 
 
-@dataclass
 class Guild:
-    id: int
-    emojis: dict[str | int, Emoji] = field(default_factory=dict)
+    def __init__(self, data: dict[str, Any]):
+        self.id: int = data["id"]
+        self.emojis: dict[str | int, Emoji] = self._parse_emojis(data["emojis"])
+
+    def _parse_emojis(self, emoji_data) -> dict[str | int, Emoji]:
+        # emoji objects are indexed by both name and id because both seem useful
+        emojis = {}
+        for emoji_data in emoji_data:
+            emoji = Emoji(self, emoji_data)
+            emojis[emoji.name] = emoji
+            emojis[emoji.id] = emoji
+        return emojis
 
     async def request_emojis(self) -> dict[str | int, Emoji]:
         request = HTTPRequest()
         response = await request.list_guild_emojis(self.id)
         if response.status_code != 200:
             return self.emojis
-        emojis = {}
-        for emoji_data in response.json():
-            emoji = Emoji(self, emoji_data)
-            emojis[emoji.name] = emoji
-            emojis[emoji.id] = emoji
-        self.emojis = emojis
+        self.emojis = self._parse_emojis(response.json())
         return self.emojis
